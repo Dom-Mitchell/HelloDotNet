8653bd45842e09453455e43053b3e83c4e0127ce
Domenick Mitchell <domenickmitchell00@gmail.com>
2021-07-26 20:04:10 -0400

    Done with Hello World

--- Program.cs
+++ Program.cs
@@ -2,11 +2,21 @@
 
 namespace HelloDotNet
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            // Variables
+            var name = "Dom Mitchell";
+            var userName = "";
+
+            // My Greeting
+            Console.WriteLine($"\nHello World, my name is {name}!\n");
+
+            // User Greeting
+            Console.WriteLine("What is your name?");
+            userName = Console.ReadLine();
+            Console.WriteLine($"\nIts a pleasure to meet you, {userName}!\n");
+        }
     }
-  }
 }
